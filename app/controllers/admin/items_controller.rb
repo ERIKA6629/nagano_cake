8fcf2ad395ddb12f87583308faacb3ff9d933d32
ERIKA6629 <151750459+ERIKA6629@users.noreply.github.com>
2024-01-24 14:23:12 +0000

    [add]item_index,show-view

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,3 +1,31 @@
 class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
+  
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    item = Item.new(item_params)
+    item.save
+    @item = Item.find(item.id)
+    redirect_to admin_item_path(item.id)
+  end
+  
+  def index
+    @items = Item.all
+  end
+  
+  def show
+    @item = Item.find(params[:id])
+    @include_tax = @item.price * 1.1
+    @include_tax = @include_tax.to_i
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price, :is_sold_out)
+  end
+  
 end
