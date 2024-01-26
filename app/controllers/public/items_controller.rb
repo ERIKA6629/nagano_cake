cec08cb35281ed7ea432efc6f86662053dbea7f7
ERIKA6629 <151750459+ERIKA6629@users.noreply.github.com>
2024-01-26 01:48:03 +0000

    [add]customer_item_index

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,9 @@
 class Public::ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+    @items_count = @items.count{|i| i.is_sold_out? === false}
+    @items = Item.page(params[:page]).per(12)
+  end
+
 end
