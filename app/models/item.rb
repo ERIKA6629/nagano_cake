ec51ac285d18967e0415142950e6e389888ff6d6
ERIKA6629 <151750459+ERIKA6629@users.noreply.github.com>
2024-01-27 13:58:05 +0000

    [add]admin-customer index-index,show,edit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   has_one_attached :image
   
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
 end
