75074a200d3aae4783ea52e5adde5c5bbd056b06
ERIKA6629 <151750459+ERIKA6629@users.noreply.github.com>
2024-01-28 13:34:14 +0000

    [add]cart_item-create,index

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,3 +1,22 @@
 class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
+  
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save!
+    redirect_to cart_items_path
+
+  end
+  
+  def index
+    @cart_item = CartItem.all
+    @total_price = 0
+  end
+  
+  private
+  
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,17 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :cart_items
          
   validates :email, uniqueness: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :email, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :telephone_number, presence: true
+  validates :last_name, presence: true
 end
