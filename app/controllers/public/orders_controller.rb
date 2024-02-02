a8c298fd867a476a8cc7236e32b6c2417881d79f
ERIKA6629 <151750459+ERIKA6629@users.noreply.github.com>
2024-02-02 14:31:25 +0000

    [add]order_confirm住所の表示

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,5 +7,23 @@ class Public::OrdersController < ApplicationController
   
   def confirm
     @orders = current_customer.cart_items
+    @order = Order.new(order_params)
+    @shipping_fee = 800
+    @total_price = 0
+    if params[:order][:address_type] == "registered_address"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:address_type] == "new_address"
+      return
+    end
+    
   end
+  
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :postal_code, :name, :address, :total_price, :payment_method, :shipping_fee)
+  end
+  
 end
