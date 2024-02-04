faf546f416b5104705c0198baf18157be769b617
ERIKA6629 <151750459+ERIKA6629@users.noreply.github.com>
2024-02-04 12:35:32 +0000

    [fix]注文ステータスの記述_status_method => status

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
-  enum status_method: { waiting_payment: 0, confirmed_payment: 1, is_producing: 2, prepareing_shipping: 3, confirmed_shipping: 4 }
+  enum status: { waiting_payment: 0, confirmed_payment: 1, is_producing: 2, prepareing_shipping: 3, confirmed_shipping: 4 }
   
   belongs_to :customer
   has_many :order_details
