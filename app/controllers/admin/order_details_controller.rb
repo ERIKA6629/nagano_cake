506f1742ed2c415ac7fdacbfaf9249022d9986f3
ERIKA6629 <151750459+ERIKA6629@users.noreply.github.com>
2024-02-06 13:59:02 +0000

    [add]ステータス_製作中の紐付け

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -4,6 +4,11 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     order_detail = OrderDetail.find(params[:id])
     order_detail.update!(order_detail_params)
+    
+    if order_detail.making_status == "in_production"
+      order = Order.find_by(id: order_detail.order_id)
+      order.update!(status: 2)
+    end
     redirect_to admin_order_path(order_detail.order_id)
   end
   
